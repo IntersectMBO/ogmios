534a7568fbbedd1f644e93fce4ecbff54eeacb64
Pablo Lamela <pablo.lamela@iohk.io>
2026-05-28 03:44:52 +0200

    Add network/tip integration test
    Compares queryNetwork/tip slot against cardano-cli query tip.

--- server/test/integration/src/Main.hs
+++ server/test/integration/src/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Test.Tasty (defaultMain, testGroup)
 import Test.Integration.Env (withTestEnv)
 import Test.Integration.LedgerTip (ledgerTipTests)
+import Test.Integration.NetworkTip (networkTipTests)
 import Test.Integration.ProtocolParameters (protocolParametersTests)
 import Test.Integration.Utxo (utxoTests)
 
@@ -13,4 +14,5 @@ main = defaultMain $
       [ utxoTests getEnv
       , protocolParametersTests getEnv
       , ledgerTipTests getEnv
+      , networkTipTests getEnv
       ]
