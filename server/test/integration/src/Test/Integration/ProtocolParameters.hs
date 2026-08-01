85b6e0c2d2b3efe858f7a52725d880f3aea69de4
Pablo Lamela <pablo.lamela@iohk.io>
2026-08-01 03:07:01 +0000

    Unify the protocol-parameters field helpers into one getField

--- server/test/integration/src/Test/Integration/ProtocolParameters.hs
+++ server/test/integration/src/Test/Integration/ProtocolParameters.hs
@@ -5,6 +5,7 @@ module Test.Integration.ProtocolParameters
   ( protocolParametersTests
   ) where
 
+import Control.Monad (foldM)
 import Data.Aeson (Value, (.:))
 import Data.Aeson.Key (Key)
 import Data.Aeson.Types (withObject)
@@ -22,28 +23,28 @@ protocolParametersTests getEnv = testGroup "ProtocolParameters"
       ogmiosResult <- queryOgmios env "queryLedgerState/protocolParameters" Nothing
       cliResult <- queryCli env ["conway", "query", "protocol-parameters"]
 
-      oMinFeeCoeff <- field1 ogmiosResult "minFeeCoefficient"
-      cMinFeeCoeff <- field1 cliResult "txFeePerByte"
+      oMinFeeCoeff <- ogmiosResult `getField` ["minFeeCoefficient"]
+      cMinFeeCoeff <- cliResult `getField` ["txFeePerByte"]
       assertEqual "minFeeCoefficient" oMinFeeCoeff cMinFeeCoeff
 
-      oMinFeeConst <- field3 ogmiosResult "minFeeConstant" "ada" "lovelace"
-      cMinFeeConst <- field1 cliResult "txFeeFixed"
+      oMinFeeConst <- ogmiosResult `getField` ["minFeeConstant", "ada", "lovelace"]
+      cMinFeeConst <- cliResult `getField` ["txFeeFixed"]
       assertEqual "minFeeConstant" oMinFeeConst cMinFeeConst
 
-      oMaxBlockBody <- field2 ogmiosResult "maxBlockBodySize" "bytes"
-      cMaxBlockBody <- field1 cliResult "maxBlockBodySize"
+      oMaxBlockBody <- ogmiosResult `getField` ["maxBlockBodySize", "bytes"]
+      cMaxBlockBody <- cliResult `getField` ["maxBlockBodySize"]
       assertEqual "maxBlockBodySize" oMaxBlockBody cMaxBlockBody
 
-      oMaxTxSize <- field2 ogmiosResult "maxTransactionSize" "bytes"
-      cMaxTxSize <- field1 cliResult "maxTxSize"
+      oMaxTxSize <- ogmiosResult `getField` ["maxTransactionSize", "bytes"]
+      cMaxTxSize <- cliResult `getField` ["maxTxSize"]
       assertEqual "maxTxSize" oMaxTxSize cMaxTxSize
 
-      oStakeDeposit <- field3 ogmiosResult "stakeCredentialDeposit" "ada" "lovelace"
-      cStakeDeposit <- field1 cliResult "stakeAddressDeposit"
+      oStakeDeposit <- ogmiosResult `getField` ["stakeCredentialDeposit", "ada", "lovelace"]
+      cStakeDeposit <- cliResult `getField` ["stakeAddressDeposit"]
       assertEqual "stakeCredentialDeposit" oStakeDeposit cStakeDeposit
 
-      oPoolDeposit <- field3 ogmiosResult "stakePoolDeposit" "ada" "lovelace"
-      cPoolDeposit <- field1 cliResult "stakePoolDeposit"
+      oPoolDeposit <- ogmiosResult `getField` ["stakePoolDeposit", "ada", "lovelace"]
+      cPoolDeposit <- cliResult `getField` ["stakePoolDeposit"]
       assertEqual "stakePoolDeposit" oPoolDeposit cPoolDeposit
   ]
 
@@ -51,11 +52,8 @@ protocolParametersTests getEnv = testGroup "ProtocolParameters"
 -- Helpers
 -- ---------------------------------------------------------------------------
 
-field1 :: Value -> Key -> IO Value
-field1 val k = parseIO ("field " <> show k) (withObject "obj" (.: k)) val
-
-field2 :: Value -> Key -> Key -> IO Value
-field2 val k1 k2 = field1 val k1 >>= \v -> field1 v k2
-
-field3 :: Value -> Key -> Key -> Key -> IO Value
-field3 val k1 k2 k3 = field2 val k1 k2 >>= \v -> field1 v k3
+-- | Drill through nested objects along a path of keys, failing on the first
+-- missing one.
+getField :: Value -> [Key] -> IO Value
+getField = foldM $ \v k ->
+  parseIO ("field " <> show k) (withObject "obj" (.: k)) v
